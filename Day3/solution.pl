d33a5413bb145a5b38a6d1141f299e3d5099e204
Philip Peter <philip.peter@justgeek.de>
2024-12-03 10:28:37 +0100

    solution to second part

--- Day3/solution.pl
+++ Day3/solution.pl
@@ -6,12 +6,21 @@ use warnings;
 my $filename = $ARGV[0];
 open(my $fh, '<', $filename) or die "Could not open file '$filename' $!";
 my $result = 0;
+my $active = 1;
 
 while (my $line = <$fh>) {
     chomp($line); # Remove trailing newline
-    while ($line =~ /mul\((\d{1,3}),(\d{1,3})\)/g){
-            print "Found a pair: $1, $2\n"; 
-            $result += $1 * $2;
+    while ($line =~ /(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))/g) {
+        my $match = $1;
+        if (defined $2 && defined $3){
+            if ( $active == 1 ) {
+                $result += $2 * $3;
+            }
+        } elsif ($match =~ /^do\(\)$/) {
+            $active = 1;
+        } elsif ($match =~ /^don't\(\)$/) {
+            $active = 0;
+        } 
     }
 }
 print "Result is $result\n";
